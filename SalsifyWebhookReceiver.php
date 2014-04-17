c49e5f57d08b3dc540279edd5b0357910f33bfb5
Rob Gonzalez <rob.gonzalez@gmail.com>
2014-04-17 09:26:57 -0400

    basic worker done

--- SalsifyWebhookReceiver.php
+++ SalsifyWebhookReceiver.php
@@ -1,41 +1,9 @@
 <?php
-
-require_once dirname(__FILE__).'/lib/Salsify_API.php';
-require_once dirname(__FILE__).'/lib/Salsify_JsonStreamingParser.php';
-
-require_once dirname(__FILE__).'/Salsify4TellAdapter.php';
-
+require("phar://iron_worker.phar");
 
 $publicationNotification = json_decode($_POST, true);
 $dataUrl = $publicationNotification['product_feed_export_url'];
 
-$salsify = new SalsifyAPI(getenv('SALSIFY_API_KEY'), getenv('SALSIFY_CHANNEL_ID'));
-
-// TODO everything following would all go into a background job
-
-$salsifyFile = tmpfile();
-
-SalsifyAPI::downloadData($dataUrl, $salsifyFile);
-fseek($salsifyFile, 0);
-
-$options = array(
-  'brand attribute ID' => 'Brand',
-  'category attribute ID' => 'Category',
-  'product page URL attribute ID' => 'Product Link',
-  'image attribute ID' => 'Image',
-  'part number attribute ID' => 'UPC'
-);
-$fourtellFile = tmpfile();
-$adapter = new Salsify4TellAdapter($fourtellFile, $options);
-$parser = new SalsifyJsonStreamingParser($salsifyFile, $adapter);
-$parser->parse();
-
-// FIXME upload the 4Tell file somewhere...
-fseek($fourtellFile, 0);
-$text = file_get_contents($fourtellFile);
-echo "\n\n" . $text . "\n\n";
-
-fclose($fourtellFile);
-fclose($salsifyFile);
-
+$worker = new IronWorker();
+$worker->postTask("adapter", array('url' => $dataUrl));
 ?>
---
+++ adapter_worker.php
@@ -0,0 +1,34 @@
+<?php
+require_once dirname(__FILE__).'/lib/Salsify_API.php';
+require_once dirname(__FILE__).'/lib/Salsify_JsonStreamingParser.php';
+
+require_once dirname(__FILE__).'/Salsify4TellAdapter.php';
+
+$payload = getPayload();
+$dataUrl = $payload('url');
+
+$salsifyFile = tmpfile();
+Salsify_API::downloadData($dataUrl, $salsifyFile);
+fseek($salsifyFile, 0);
+
+$options = array(
+  'brand attribute ID' => 'Brand',
+  'category attribute ID' => 'Category',
+  'product page URL attribute ID' => 'Product Link',
+  'image attribute ID' => 'Image',
+  'part number attribute ID' => 'UPC'
+);
+$fourtellFile = tmpfile();
+$adapter = new Salsify4TellAdapter($fourtellFile, $options);
+$parser = new Salsify_JsonStreamingParser($salsifyFile, $adapter);
+$parser->parse();
+
+// FIXME upload the 4Tell file somewhere...
+fseek($fourtellFile, 0);
+$text = file_get_contents($fourtellFile);
+echo "\n\n" . $text . "\n\n";
+
+fclose($fourtellFile);
+
+fclose($salsifyFile);
+?>
